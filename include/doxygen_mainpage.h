12f032f4a158e573173deedf794ae3686182c02b
Yoichi Izumi <quanyangyi0323@gmail.com>
2010-11-24 20:19:46 +0900

    init

---
+++ include/doxygen_mainpage.h
@@ -0,0 +1,17 @@
+#ifndef _DOXYGEN_MAINPAGE_H_
+#define _DOXYGEN_MAINPAGE_H_
+
+/**
+ *
+ * @mainpage テンプレート
+ * @version 0.1
+ *
+ * @section overview_sec 概要
+ * これはC言語用のテンプレートプロジェクトです。
+ * includeの下に.hファイル群を、
+ * srcの下に.cファイル群を、
+ * specの下にCSpecを使ったテストコードを、
+ * それぞれ作ってください。
+ */
+
+#endif
---
+++ spec/spec_runner.c
@@ -0,0 +1,14 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <testRunner.h>
+
+#ifdef WITH_DMALLOC
+# include <dmalloc.h>
+#endif
+
+int main()
+{
+    return 0;
+}
